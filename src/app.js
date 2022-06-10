7fee8fe48c59e5377a1111cca118062d6f2e1f60
Neto (João Araújo) <Bipnerds@gmail.com>
2022-06-10 09:09:57 -0300

    Remove Rectangle

--- src/app.js
+++ src/app.js
@@ -7,12 +7,7 @@ let screen_height = 720;
 let app = new PIXI.Application({ width: screen_width, height: screen_height });
 document.body.appendChild(app.view);
 
-const graphics = new PIXI.Graphics();
-graphics.beginFill(0xFFFFFF);
-graphics.drawRect(10, 10, 100, 100);
-graphics.endFill();
-
 let eball = new Ball(screen_width / 2, screen_height / 2, 10, 0xFFFFFF);
 eball.draw();
 
-app.stage.addChild(eball._graphics, graphics);
+app.stage.addChild(eball._graphics);
