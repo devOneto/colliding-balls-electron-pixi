6183c915ad6de52292b77fc7e9f86e06c93b305d
Neto (João Araújo) <Bipnerds@gmail.com>
2022-06-10 09:36:57 -0300

    Adding Game Loop and Renderer

--- src/app.js
+++ src/app.js
@@ -4,10 +4,34 @@ import { Ball } from './game/ball.js'
 let screen_width = 1280;
 let screen_height = 720;
 
-let app = new PIXI.Application({ width: screen_width, height: screen_height });
-document.body.appendChild(app.view);
+let renderer = new PIXI.Renderer({ width: screen_width, height: screen_height, backgroundColor: 0x1099bb });
+document.body.appendChild(renderer.view);
+
+let stage = new PIXI.Container();
+
+let old_time = Date.now();
+
+requestAnimationFrame(animate);
+function animate() {
+
+    let new_time = Date.now();
+    let delta_time = new_time - old_time;
+
+    old_time = new_time;
+    if (delta_time < 0) delta_time = 0;
+    if (delta_time > 1000) delta_time = 1000;
+    let delta_frame = delta_time * 60 / 1000;
+
+    // game update
+    // 
+
+    renderer.render(stage);
+
+    requestAnimationFrame(animate);
+}
+
 
 let eball = new Ball(screen_width / 2, screen_height / 2, 10, 0xFFFFFF);
 eball.draw();
 
-app.stage.addChild(eball._graphics);
+stage.addChild(eball._graphics);
